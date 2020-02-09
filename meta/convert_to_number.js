3404af44ffe062a65617bab779f5c6eee03e85f1
iammadab <iammadab@gmail.com>
2020-02-10 00:50:16 +0100

    Implemented the function to convert a string to a number

--- meta/convert_to_number.js
+++ meta/convert_to_number.js
@@ -1,5 +1,19 @@
+/* 
+	This function converts a string to its number representation based on the ascii character code
+	Each ascii character is padded with zeros unilt its length is 3 characters, so as to allow for easy conversion back to strings
+*/
 function convertToNumber(string){
-
+	let characters = string.split("")
+	let numberString = characters.map(character => {
+		let asciiValue = ascii(character), paddedValue = String(asciiValue).padStart(3, "0")
+		return paddedValue
+	}).join("")
+	return numberString
 }
 
 module.exports = convertToNumber
+
+
+function ascii(character){
+	return character.charCodeAt(0)
+}
---
+++ meta/convert_to_string.js
@@ -0,0 +1,5 @@
+function convertToString(number){
+
+}
+
+module.exports = convertToString
