58be25b5c743cacf8e2052f86b39caf83b2fcba8
iammadab <iammadab@gmail.com>
2020-02-10 00:26:40 +0100

    Defined the high level algorithm for breakSecret

--- api/break_secret.js
+++ api/break_secret.js
@@ -1,5 +1,8 @@
 function breakSecret({ secretString, piecesCount, threshold }){
-
+	let secretNumber = convertToNumber(secretString)
+	let polynomial = createPolynomial(secretNumber, threshold)
+	let pieces = generatePieces(polynomial, piecesCount)
+	return pieces
 }
 
 module.exports = breakSecret
