2ebdaa3ffff912aa58c23ab7444d1e28aa9fe622
iammadab <iammadab@gmail.com>
2020-02-10 00:28:14 +0100

    Defined the high level algorithm for reconstructSecret

--- api/reconstruct_secret.js
+++ api/reconstruct_secret.js
@@ -1,5 +1,8 @@
 function reconstructSecret({ pieces }){
-
+	let points = piecesToPoint(pieces)
+	let intercept = calculateIntercept(points)
+	let secret = convertToString(intercept)
+	return secret
 }
 
 module.exports = reconstructSecret
