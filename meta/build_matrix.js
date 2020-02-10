982b20ba1c1f052e1b11b6cdbf826604412ccd6d
iammadab <iammadab@gmail.com>
2020-02-10 12:30:45 +0100

    Implemented the build matrix function

---
+++ meta/build_matrix.js
@@ -0,0 +1,33 @@
+const BigNumber = require("bignumber.js")
+
+/*
+	The reconstruction process involves solving a system of linear equations
+	This function takes the points provided and build a matrix that represents the system of equations
+*/
+
+function buildMatrix(points){
+	let threshold = points.length, 
+			matrix = points.map(point => generateRow(point, threshold))
+	return matrix
+}
+
+module.exports = buildMatrix
+
+function generateRow(point, threshold){
+	let row = []
+	for(let pos = 0; pos < threshold; pos++){
+		row.push(computeTerm(point.position, pos))
+	}
+	return row
+}
+
+// This computes the co-efficient of each of the constants a0,a1,...an
+function computeTerm(point, position){
+	let term = Math.pow(point, position)
+	return new BigNumber(term)
+}
+
+// console.log(buildMatrix([ 
+// 	{ position: 5},
+// 	{ position: 10 }
+// ]))
