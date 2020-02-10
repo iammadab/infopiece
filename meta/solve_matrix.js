2e4cd793d7e7a5913f13c69ed556fd0b308adcd8
iammadab <iammadab@gmail.com>
2020-02-10 13:37:09 +0100

    Extended big number and implemented the solve matrix function

--- meta/solve_matrix.js
+++ meta/solve_matrix.js
@@ -1,7 +1,10 @@
+// Extend bignumber to include the cmp and sub functions
 const BigNumber = require("bignumber.js")
+BigNumber.prototype.cmp = BigNumber.prototype.comparedTo
+BigNumber.prototype.sub = BigNumber.prototype.minus
+
 const GaussianElimination = require("na-gaussian-elimination")
 const zero = new BigNumber(0)
-
 GaussianElimination.defaultOptions.zero = zero
 
 
@@ -11,10 +14,10 @@ function solveMatrix(matrix, result){
 	return system.solution
 }
 
-// solveMatrix(
+// console.log(solveMatrix(
 // 	[
 // 		[new BigNumber(1), new BigNumber(1)],
 // 		[new BigNumber(1), new BigNumber(2)]
 // 	], 
 // 	[new BigNumber(535), new BigNumber(745)]
-// )
+// ))
