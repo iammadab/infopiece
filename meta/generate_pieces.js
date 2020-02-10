cde0efa730e2f5f7ef64f32d8b41532170c71ce9
iammadab <iammadab@gmail.com>
2020-02-10 01:54:14 +0100

    Added more descriptive comments in the generate pieces function

--- meta/generate_pieces.js
+++ meta/generate_pieces.js
@@ -12,11 +12,15 @@ function generatePieces(polynomial, piecesCount){
 
 module.exports = generatePieces
 
+// This is equivalent to passing a value x in a polynomial
 function solvePolynomialFor(polynomial, x){
 	let values = polynomial.map((term, pos) => computeTerm(pos, term, x))
 	return values.reduce((acc, curr) => acc + curr, 0)
 }
 
+// A polynomial has terms separted by an operator e.g +, each of those terms return different values
+// This function, calculates the value that should be returned based on the position in the polynomial,
+// the value of the term and the value of x in the polynomial
 function computeTerm(position, term, x){
 	return term * Math.pow(x, position)
 }
