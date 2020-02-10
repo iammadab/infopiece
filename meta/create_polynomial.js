bf5776f5556eacfbdaac7406a3ad9b51a9c8a660
iammadab <iammadab@gmail.com>
2020-02-10 01:38:00 +0100

    Implemented the function to create polynomial

---
+++ meta/create_polynomial.js
@@ -0,0 +1,27 @@
+/*
+	This function creates a polynomial by returning an array whose elementes represent the co-efficients a0,a1,a2,...,an
+	The polynomial will be used to generate the pieces later on
+	The conversion of the secret to a number might remove zeros (0) at the beginning of the secret
+	This should be taken note of inorder to avoid conversion errors
+
+	An improvement will be to distribute the polynomial over a finite field so as to avoid floating point issues
+*/
+
+function createPolynomial(secretNumber, threshold){
+	let secret = Number(secretNumber), polynomial = [secret, ...guess(secret, threshold)]
+	return polynomial
+}
+
+module.exports = createPolynomial
+
+function guess(secret, threshold){
+	let guesses = []
+	for(let i = 0; i < threshold; i++){
+		guesses.push(random(secret))
+	}
+	return guesses
+}
+
+function random(number){
+	return Math.floor(Math.random() * number)
+}
