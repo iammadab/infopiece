727ca5e3c263be1c010753a104589b7a8c8d1412
iammadab <iammadab@gmail.com>
2020-02-10 16:19:47 +0100

    Refactored generate points to use big number

--- meta/generate_points.js
+++ meta/generate_points.js
@@ -1,3 +1,4 @@
+const BigNumber = require("bignumber.js")
 /*
 	This function takes a polynomial, and computes the values of the polynomial for values 1,2,3,...,piecesCount
 	All these values are returned in an array
@@ -15,14 +16,14 @@ module.exports = generatePoints
 // This is equivalent to passing a value x in a polynomial
 function solvePolynomialFor(polynomial, x){
 	let values = polynomial.map((term, pos) => computeTerm(pos, term, x))
-	return values.reduce((acc, curr) => acc + curr, 0)
+	return values.reduce((acc, curr) => acc.plus(curr), new BigNumber(0))
 }
 
 // A polynomial has terms separted by an operator e.g +, each of those terms return different values
 // This function, calculates the value that should be returned based on the position in the polynomial,
 // the value of the term and the value of x in the polynomial
 function computeTerm(position, term, x){
-	return term * Math.pow(x, position)
+	return term.multipliedBy(Math.pow(x, position))
 }
 
 // console.log(generatePoints([325, 210], 4))
