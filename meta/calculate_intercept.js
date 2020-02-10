2abcf438634a08946a6f4f3b5fee19d3950bd72e
iammadab <iammadab@gmail.com>
2020-02-10 11:10:14 +0100

    Implemented the extract result function

---
+++ meta/calculate_intercept.js
@@ -0,0 +1,16 @@
+const BigNumber = require("bignumber.js")
+
+function calculateIntercept(points){
+	let matrix = buildMatrix(points), result = extractResult(points)
+	let solvedMatrix = solveMatrix(matrix, result)
+	return grabIntercept(solveMatrix)
+}
+
+module.exports = calculateIntercept
+
+
+function extractResult(points){
+	return points.map(point => {
+		return new BigNumber(point.point)
+	})
+}
